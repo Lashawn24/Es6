f2178d55757238a76b3478a4301bee51346acc7c
Lashawn <lashawnwood23@gmail.com>
2022-04-18 08:57:47 -0400

    push

---
+++ app.js
@@ -0,0 +1,41 @@
+var a = 40;
+var b = 50;
+
+function addSum(param1, param2){
+    console.log(param1 + param2);}
+addSum(a, b);
+
+const AddSum = (param1, param2) => {
+    console.log(param1 + param2);}
+AddSum(a,b);
+
+
+function subtractSum(param1, param2) {
+    console.log(param1 - param2);}
+subtractSum(a,b);
+
+const SubtractSum =(param1, param2)=> {
+    console.log(param1 - param2);}
+    SubtractSum(a,b);
+
+
+function multiplySum(param1, param2) {
+    console.log(param1 * param2);}
+multiplySum(a, b);
+
+
+const MultiplySum = (param1, param2) => {
+    console.log(param1 * param2);}
+multiplySum(a, b);
+
+
+
+
+function divideSum(param1, param2) {
+    console.log(param1 / param2);}
+    divideSum(a, b);
+
+const DivideSum = (param1, param2) => {
+    console.log(param1 / param2);}
+    divideSum(a, b);
+
